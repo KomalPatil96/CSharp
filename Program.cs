58c5a7a8f97597a3671e2129bb08bb118d8012df
KomalPatil96 <137880313+KomalPatil96@users.noreply.github.com>
2023-06-29 18:51:25 +0530

    How to create object

--- Program.cs
+++ Program.cs
@@ -4,28 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Arrays1
+namespace objectCreation
 {
-    class Program
+    class car
     {
+        string color = "Red";
+        int maxspeed = 200;
         static void Main(string[] args)
         {
-            string[] cars = new string[4] { "Maruti", "BMW", "Ford", "Ferrari" };
-            Console.WriteLine(cars[2]);
-            Console.WriteLine("\n");
+            car mycar = new car();
+            Console.WriteLine(mycar.maxspeed);
 
-            for (int i = 0; i < cars.Length; i++)
-            {
-                Console.WriteLine(cars[i]);
-
-            }
-            Console.WriteLine("\n");
-
-            foreach (string car in cars)
-            {
-                Console.WriteLine(car);
-            }
+            car2 mycar2 = new car2();
+            Console.WriteLine(mycar2.color);
             Console.ReadLine();
         }
+    }
+
+    class car2
+    {
+        public string color = "Red";
     }
 }
