af9959423f10a633df5d78aa21956452220a8a61
KomalPatil96 <137880313+KomalPatil96@users.noreply.github.com>
2023-07-04 22:49:45 +0530

    Encapsulation

---
+++ Encapsulation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Encapsulation_demo
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                home h = new home();
+                h.demo();
+            }
+        }
+
+        class home
+        {
+            int i;
+            protected int j;
+
+            public void demo()
+            {
+                Console.Write("\n\n\t Value of i = {0} & value of j = {1} ");
+                Console.ReadLine();
+            }
+           
+        }
+    }
+}
