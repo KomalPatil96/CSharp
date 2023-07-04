723707f2e888393ad95c6ab3775084e441db1cfe
KomalPatil96 <137880313+KomalPatil96@users.noreply.github.com>
2023-07-04 22:52:00 +0530

    Write a code for Enum

---
+++ Enum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumDemo
+{
+    class Enum
+    {
+        enum level
+        {
+            low, medium, high
+        }
+        static void Main(string[] args)
+        {
+            level mylevel = level.high;
+            Console.WriteLine(mylevel);
+            Console.ReadLine();
+    
+        }
+    }
+
+    //enum level
+    //{
+    //    low,medium,high
+    //}
+
+}
