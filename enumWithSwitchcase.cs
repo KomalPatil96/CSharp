bcf57f31e922093c1cb255533ac8c0615b1c303a
KomalPatil96 <137880313+KomalPatil96@users.noreply.github.com>
2023-07-04 22:55:33 +0530

    enum

---
+++ enumWithSwitchcase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumWithSwitchcase
+{
+    class enumWithSwitchcase
+    {
+        static void Main(string[] args)
+        {
+            level myvar = level.medium;
+            Console.WriteLine(myvar);
+
+            switch(myvar)
+            {
+                case level.low:
+                    Console.WriteLine("Level is low.");
+                    break;
+
+                case level.medium:
+                    Console.WriteLine("Level is medium.");
+                    break;
+
+                case level.high:
+                    Console.WriteLine("Level is high.");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+
+    enum level
+    {
+        low,medium,high
+    }
+}
