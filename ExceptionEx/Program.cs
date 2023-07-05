a1efc7a34304f2438d2a9e0014bc89b5d0326395
KomalPatil96 <137880313+KomalPatil96@users.noreply.github.com>
2023-07-05 22:34:07 +0530

    Add files via upload

---
+++ ExceptionEx/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace exceptionAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Directory.GetDirectories("D:\\shri\\");
+            }
+            catch(DirectoryNotFoundException de)
+            {
+                Console.WriteLine(de.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
