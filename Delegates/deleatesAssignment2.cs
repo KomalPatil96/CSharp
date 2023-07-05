4dc0b6bed4f90b5d229501dfe7ec27c2eff672ef
KomalPatil96 <137880313+KomalPatil96@users.noreply.github.com>
2023-07-05 22:28:43 +0530

    Assignment2

---
+++ Delegates/deleatesAssignment2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+delegate int renew(int n);
+
+namespace deleatesAssignment2
+{
+    class deleatesAssignment2
+    {
+        static int num = 10;
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            renew nc;
+            renew nc1 = new renew(AddNum);
+            renew nc2 = new renew(MultNum);
+            nc = nc1;
+            nc += nc2;
+            
+            nc(4);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
+        }
+    }
+}
